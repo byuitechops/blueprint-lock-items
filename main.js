01c9dec270501a4b96f54d0a943da76f46a2c8c0
Danverde <gre12014@byui.edu>
2018-01-12 16:12:42 -0700

    grabs & filters assignments

--- main.js
+++ main.js
@@ -1,28 +1,89 @@
 /*eslint-env node, es6*/
 
 /* Module Description */
+/* locks items of blueprint courses */
 
 /* Put dependencies here */
-
-/* Include this line only if you are going to use Canvas API */
-// const canvas = require('canvas-wrapper');
-
-/* View available course object functions */
-// https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
+const canvas = require('canvas-wrapper'),
+    asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('moduleName');
+    course.addModuleReport('blueprint-lock-items');
 
-    /* Used to log successful actions */
-    course.success('moduleName', 'moduleName successfully ...');
+    /* only run if the current course is a blueprint course & locking by obj type is enabled */
+    if (!course.info.isBlueprint || !course.info.lockByObj) {
+        course.success('blueprint-lock-items', 'Determined this course is not a blueprint');
+        stepCallback(null, course);
+        return;
+    }
 
-    /* How to report an error (Replace "moduleName") */
-    // course.throwErr('moduleName', e);
+    function lockItems(items, itemType, cb) {
+        asyncLib.eachLimit(items, (item, itemCb) => {
 
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
+            canvas.put(`/api/v1/courses/${course.info.canvasOU}/blueprint_templates/default/restrict_item`, {
+                    'content_type': itemType.type,
+                    'content_id': item.id,
+                    'restricted': true
+                },
+                (err, res) => {
+                    if (err) {
+                        itemCb(err);
+                        return;
+                    }
+                    itemCb(null);
+                });
+        }, (err) => {
+            if (err) {
+                cb(err);
+                return;
+            }
+            cb(null);
+        });
+    }
 
-    stepCallback(null, course);
+    function setup(itemType, cb) {
+        itemType.getter(course.info.canvasOU, (err, items) => {
+            if (err) {
+                // do we really want to do this here?
+                cb(err);
+                return;
+            }
+            var keep;
+            var filteredItems = items.filter((item) => {
+                keep = true;
+                unlockedNames.forEach((regex) => {
+                    if (regex.test(item[itemType.name]) || item.restricted_by_master_course == true)
+                        keep = false;
+                });
+                return keep;
+            }).map((item) => {
+                return {
+                    name: item[itemType.name],
+                    id: item[itemType.id]
+                }
+            });
+            // console.log(JSON.stringify(filteredItems, null, 2));
+
+            lockItems(filteredItems, itemType, cb);
+        });
+    }
+
+
+    var unlockedNames = [/notes\s*from\s*instructor*/i];
+    var toLock = [{
+        type: 'assignment',
+        getter: canvas.getAssignments,
+        name: 'name',
+        id: 'id'
+    }];
+
+    /* get all assignments */
+    asyncLib.eachSeries(toLock, setup, (err) => {
+        if (err) {
+            course.throwErr('blueprint-lock-items', err);
+            stepCallback(null, err);
+        }
+        console.log('done');
+        stepCallback(null, course);
+    });
 };
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Description #
+This is a child module of the d2l-to-canvas-conversion tool. It is a postImport tool which locks all pages, files, assignments, quizzes, and discussion boards except "notes from instructor". It is reccommended that this child module run towards the end of the conversion.
